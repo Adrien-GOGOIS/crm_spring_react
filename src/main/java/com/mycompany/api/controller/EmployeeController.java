34291359983fbfeeb716f264a149db843c154dd5
Adrien GOGOIS <ad.gogois@gmail.com>
2023-01-16 10:29:31 +0100

    Add create, delete, getById endpoints

--- src/main/java/com/mycompany/api/controller/EmployeeController.java
+++ src/main/java/com/mycompany/api/controller/EmployeeController.java
@@ -3,8 +3,9 @@ package com.mycompany.api.controller;
 import com.mycompany.api.model.Employee;
 import com.mycompany.api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -18,5 +19,21 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        return employee.orElse(null);
+    }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
     }
 }
--- src/test/java/com/mycompany/api/controller/EmployeeControllerTest.java
+++ src/test/java/com/mycompany/api/controller/EmployeeControllerTest.java
@@ -1,14 +1,22 @@
-package com.mycompany.api;
+package com.mycompany.api.controller;
 
+import com.mycompany.api.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,7 +28,13 @@ public class EmployeeControllerTest {
     public MockMvc mockMvc;
 
     @Test
-    public void should_return_OK_status_when_get_employees() throws Exception{
+    public void should_return_OK_status_when_get_employees() throws Exception {
         mockMvc.perform(get("/employees")).andExpect(status().isOk()).andExpect(jsonPath("$[0].firstName", is("Laurent")));
     }
+
+    @Test
+    public void should_return_OK_status_when_get_employee_by_id() throws Exception {
+        mockMvc.perform(get("/employees/2")).andExpect(status().isOk()).andExpect(jsonPath("$.firstName", is("Sophie")));
+    }
+
 }
